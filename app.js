7a1ab27670154fdfd1ae96eb19a2c4de8af37ace
sggb <shangggb@joyowo.com>
2017-06-09 17:27:57 +0800

    modified app.js

--- app.js
+++ app.js
@@ -15,13 +15,23 @@ app.get('/city', function(req, res) {
 app.get("/findCity", function(req, res) {
 
   var param = '北京'
-
+  var result = []
   var cityJson = JSON.parse(fs.readFileSync('city-data.json', 'utf8'))
   cityJson.forEach(function(entry){
     proviceList = entry.list
     proviceList.forEach(function(provice){
       if(provice.name == param) {
-        res.send(provice.children)
+        var r = provice.name
+        var cityList = provice.children[0].list
+        cityList.forEach(function(city){
+          r = r.concat(',', city.name)
+          var areaList = city.children[0].list
+          areaList.forEach(function(area) {
+            var v = r.concat(',', area.name)
+            result.push(v)
+          })
+        })
+        res.send(result)
       }
     })
   })
